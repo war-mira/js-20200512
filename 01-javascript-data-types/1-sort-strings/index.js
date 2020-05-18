af4f5417dc0d74dd1603e260fb75270f3847deff
war_mira <tvixa40@gmail.com>
2020-05-18 23:43:43 +0600

    - sort task solution

--- 01-javascript-data-types/1-sort-strings/index.js
+++ 01-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,7 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    arr.sort( (a, b) => a.localeCompare(b, 'ru', {ignorePunctuation: true, caseFirst: 'upper'}));
 
+    return param === 'asc' ? arr : arr.reverse()
 }
