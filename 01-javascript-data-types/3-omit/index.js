b5c6f6eeacbaa8f7f0cee5983db25fe394ea61b4
war_mira <tvixa40@gmail.com>
2020-05-19 02:29:54 +0600

    - 3 task resolved

--- 01-javascript-data-types/3-omit/index.js
+++ 01-javascript-data-types/3-omit/index.js
@@ -6,4 +6,11 @@
  */
 export const omit = (obj, ...fields) => {
 
+    const filtered = Object.entries(obj).filter(([key, value], index, array)=>{
+        if (![...fields].includes(key)) {
+            return array;
+        }       
+    });
+
+    return filtered.length ? Object.fromEntries(filtered) : {}
 };
