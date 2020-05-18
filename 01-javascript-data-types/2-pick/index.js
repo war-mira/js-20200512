ce24604d7d6afc76ddb6ad95df4b0a9e1300bfad
war_mira <tvixa40@gmail.com>
2020-05-19 00:51:23 +0600

    - search in array solution

--- 01-javascript-data-types/2-pick/index.js
+++ 01-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+    const result = {}
 
+    for (let [key, value] of Object.entries(obj)) {
+        if([...fields].includes(key)){
+           result[key] = value
+        }
+    }
+    
+    return result
 };
