f5752089324d022766fa17bdc33715daba086ee2
war_mira <tvixa40@gmail.com>
2020-05-21 03:00:11 +0600

    - second lesson homework done

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,11 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathToArray = path.split('.')
 
+    return function valueFromPath (obj){
+        return pathToArray.reduce((obj, val) => {
+            return (obj ? obj[val] : undefined)
+        }, obj)
+    }
 }
--- 02-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 02-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,14 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+    if(!obj){
+        return
+    }
+    const newObject = {}
 
+    for(let [key, value] of Object.entries(obj)){
+        newObject[value] = key
+    }
+    
+    return newObject
 }
