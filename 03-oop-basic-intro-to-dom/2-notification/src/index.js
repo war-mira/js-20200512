becb1f2de16d48011eeafaddc4425e8df5d55c4e
war_mira <tvixa40@gmail.com>
2020-05-26 01:15:01 +0600

    - main task of lesson 3 done

--- 03-oop-basic-intro-to-dom/2-notification/src/index.js
+++ 03-oop-basic-intro-to-dom/2-notification/src/index.js
@@ -1,3 +1,49 @@
 export default class NotificationMessage {
+    element;
+    wrapper;
+
+    constructor(msg, obj){
+        this.text = msg ? msg : 'Пустое сообщение'
+        this.duration = obj ? obj.duration : 2000
+        this.sec = obj ? ((obj.duration) / 1000) : 2
+        this.type = obj ? obj.type : 'error'
+
+        this.render()
+    }
+
+    get template() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.sec}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                    <div class="notification-header">${this.type}</div>
+                    <div class="notification-body">
+                        ${this.text}
+                    </div>
+                </div>
+            </div>`
+        ;
+    }
+
+    render(){
+        const element = document.createElement('div');
+        element.innerHTML = this.template;
+
+        this.element = element.firstElementChild;
+    }
+
+    show(container){
+        this.wrapper = container ? container : document.body
+        this.wrapper.append(this.element)
+
+        setTimeout(() => this.remove(), this.duration)
+    }
+    remove () {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 
 }
